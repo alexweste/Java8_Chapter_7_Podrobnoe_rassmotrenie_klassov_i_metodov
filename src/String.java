37a0c8aff947252aa4f4af1393ea6a57b5d2f7c0
alexweste <alexandrzvarygin@gmail.com>
2018-04-10 22:27:31 +0300

    пример класса String

---
+++ src/String.java
@@ -0,0 +1,8 @@
+// пример класса String
+class StrClass {
+    public static void main(String[] args) {
+
+        String myString = "Это тестовая строка.";
+        System.out.println(myString);
+    }
+}
